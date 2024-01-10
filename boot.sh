c86dae2683bb4b986d636f90dd99c2e4e2a73ab0
swmwy <149204419+swmwy@users.noreply.github.com>
2024-01-10 14:13:43 -0500

    Create boot.sh and add logo

---
+++ boot.sh
@@ -0,0 +1,15 @@
+gcc main.c -o snowshell
+
+echo "Thank you for using SnowShell!"
+echo "
+                                       
+ _____               _____ _       _ _ 
+|   __|___ ___ _ _ _|   __| |_ ___| | |
+|__   |   | . | | | |__   |   | -_| | |
+|_____|_|_|___|_____|_____|_|_|___|_|_|
+                                       
+"
+
+
+
+./snowshell
